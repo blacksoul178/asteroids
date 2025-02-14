a329ad7e93f6789d45ea463cbb50afd3179a463b
Marc-André <blacksoul178@gmail.com>
2025-02-14 12:21:33 -0500

    Create Triangle, and implement rotation

--- main.py
+++ main.py
@@ -2,33 +2,34 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-pygame.init()
 from constants import *
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-background = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
-
-
-
-
+from player import Player
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        background.fill((0, 0, 0))
 
+        player.update(dt)    
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        
 
-
-        pygame.display.flip
+        #Limit Framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
