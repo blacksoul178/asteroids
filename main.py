9c6fece70b01d61cf61a742ee38a88619cf3eb40
Marc-André <blacksoul178@gmail.com>
2025-02-13 23:40:14 -0500

    Gitignore and basics

--- main.py
+++ main.py
@@ -2,13 +2,33 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+pygame.init()
 from constants import *
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+background = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+
+
+
+
+
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    while True:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        background.fill((0, 0, 0))
+
+
+
+        pygame.display.flip
 
 
 if __name__ == "__main__":
